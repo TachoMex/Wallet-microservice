c82bf96507da09b951a32725384bfd3a35067dd7
Gilberto Vargas <tachoguitar@gmail.com>
2018-02-03 13:42:21 -0600

    Add table for transactions

---
+++ models/migrations/002_add_transactions_table.rb
@@ -0,0 +1,11 @@
+Sequel.migration do
+  up do
+    create_table(:transaction) do
+      primary_key :transaction_id, auto_increment: true
+      foreign_key :wallet_id, :wallet
+      Double      :amount, null: false
+      DateTime    :entry_date, null: false, default: :now.sql_function
+      DateTime    :confirmation_date, default: nil
+    end
+  end
+end
