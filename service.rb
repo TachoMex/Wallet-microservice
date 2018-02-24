dad9f57f984b019c3511ab56a9116f8cfe375f52
Gilberto Vargas <tachoguitar@gmail.com>
2018-02-24 11:12:03 -0600

    Build API definition

--- service.rb
+++ service.rb
@@ -2,6 +2,22 @@ require 'cute_logger'
 require 'grape'
 require 'sequel'
 require 'puma'
+require 'ant'
+
+
+require_relative 'lib/services'
+require_relative 'routes/wallets'
+require_relative 'routes/transactions'
 
 class WalletService < Grape::API
+  Controller::Wallet.register(:database, Services.database[:wallet])
+  Controller::Transaction.register(:database, Services.database[:transaction])
+
+  version('v1', using: :header, vendor: :cut)
+  prefix(:api)
+  format(:json)
+
+  helpers Ant::Response
+  mount Routes::Wallets
+  mount Routes::Transactions
 end
