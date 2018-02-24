8639484d41c9d87dbf584e339e44d41305a8a401
Gilberto Vargas <tachoguitar@gmail.com>
2018-02-24 13:00:43 -0600

    Add unit test for wallet creation

---
+++ test/test_routes/test_wallets.rb
@@ -0,0 +1,29 @@
+require './test/test_helper'
+
+class TestWallets < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    WalletService
+  end
+
+  def setup
+    @database = Services.database
+    @database.run('BEGIN;')
+  end
+
+  def teardown
+    @database.run('ROLLBACK;')
+  end
+
+  def test_create_wallet
+    post('/api/wallets')
+    assert_equal(201, last_response.status)
+    response = last_json_response
+    assert_equal(response['status'], 'success')
+    refute_nil(response['data'])
+    refute_nil(response['data']['wallet_id'])
+    assert_equal(0, response['data']['balance'])
+    assert_equal(Time.now.to_s, response['data']['creation_date'])
+  end
+end
