c666909c197194cf6af4877efb4504cf5e229018
Gilberto Vargas <tachoguitar@gmail.com>
2018-03-03 11:35:10 -0600

    Refactor unit tests

--- test/test_routes/test_wallets.rb
+++ test/test_routes/test_wallets.rb
@@ -16,65 +16,67 @@ class TestWallets < Minitest::Test
     @database.run('ROLLBACK;')
   end
 
+  def assert_jsend_status(status = 'success')
+    response = last_json_response
+    assert_equal(response['status'], status)
+    refute_nil(response['data'])
+    assert_equal(2, last_response.status / 100)
+    response['data']
+  end
+
   def test_create_wallet
     post('/api/wallets')
-    assert_equal(201, last_response.status)
-    response = last_json_response
-    assert_equal(response['status'], 'success')
-    refute_nil(response['data'])
-    refute_nil(response['data']['wallet_id'])
-    assert_equal(0, response['data']['balance'])
-    assert_equal(Time.now.to_s, response['data']['creation_date'])
-    response['data']['wallet_id']
+    response = assert_jsend_status
+    refute_nil(response['wallet_id'])
+    assert_equal(0, response['balance'])
+    assert_equal(Time.now.to_s, response['creation_date'])
+    response['wallet_id']
   end
 
   def test_receive_money
     wallet_id = test_create_wallet
     post("/api/wallets/#{wallet_id}/receive_money", amount: 100)
-    response = last_json_response
-    assert_equal(201, last_response.status)
-    assert_equal(response['status'], 'success')
-    assert_nil(response['data']['confirmation_date'])
-    assert_equal(response['data']['wallet_id'], wallet_id)
-    ap(response)
+
+    response = assert_jsend_status
+    assert_nil(response['confirmation_date'])
+    assert_equal(response['wallet_id'], wallet_id)
   end
 
   def test_receive_money_float
     wallet_id = test_create_wallet
     post("/api/wallets/#{wallet_id}/receive_money", amount: 100.2324)
-    response = last_json_response
-    assert_equal(201, last_response.status)
-    assert_equal(response['status'], 'success')
-    assert_nil(response['data']['confirmation_date'])
-    assert_equal(response['data']['wallet_id'], wallet_id)
-    ap(response)
+
+    response = assert_jsend_status
+    assert_nil(response['confirmation_date'])
+    assert_equal(response['wallet_id'], wallet_id)
   end
 
   def test_fail_receive_money
     wallet_id = test_create_wallet
     post("/api/wallets/#{wallet_id}/receive_money", amount: -100)
-    response = last_json_response
-    assert_equal(response['status'], 'fail')
-    ap(response)
+    assert_jsend_status('fail')
   end
 
   def test_fatal_receive_money
-    post("/api/wallets/-1/receive_money", amount: -100)
-    response = last_json_response
-    ap(response)
-    assert_equal(response['status'], 'fail')
+    post('/api/wallets/-1/receive_money', amount: 100)
+    assert_jsend_status('fail')
   end
-  
-  def test_take_wallet
+
+  def test_take_money
     id = test_create_wallet
-    post("api/wallets/#{id}/receive_money",amount: 50)
-    assert_equal(201,last_response.status)
+    post("api/wallets/#{id}/receive_money", amount: 50)
     post("api/wallets/#{id}/take_money", amount: 20)
-    response = last_json_response
-    assert_equal('success',response['status'])
-    refute_nil(response['data'])
-    refute_nil(response['data']['amount'])
-    ap response
-    assert_equal(-20, response['data']['amount'])
+    response = assert_jsend_status
+    assert_equal(-20, response['amount'])
+
+    get("api/wallets/#{id}")
+    data = assert_jsend_status
+    assert_equal(data['balance'], 30)
+  end
+
+  def test_take_money_not_enough
+    id = test_create_wallet
+    post("api/wallets/#{id}/take_money", amount: 20)
+    assert_jsend_status('error')
   end
 end
