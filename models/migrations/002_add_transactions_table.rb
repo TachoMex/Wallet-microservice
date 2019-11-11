56ad04b83efcac316f90b04d98f958d99017b12f
Gilberto Vargas <tachoguitar@gmail.com>
2019-11-10 23:39:33 -0600

    add status field to transactions table

--- models/migrations/002_add_transactions_table.rb
+++ models/migrations/002_add_transactions_table.rb
@@ -6,6 +6,7 @@ Sequel.migration do
       Double      :amount, null: false
       DateTime    :entry_date, null: false, default: :now.sql_function
       DateTime    :confirmation_date, default: nil
+      String      :status, size: 10, null: false, default: 'pending'
     end
   end
 end
